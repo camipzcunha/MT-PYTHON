a2c2825410e7b2f006dd9ecf1793e15e43f86967
Camila Pedroza <156383341+camilapzcunha@users.noreply.github.com>
2024-03-05 22:44:47 -0300

    Add files via upload

---
+++ lista.py
@@ -0,0 +1,41 @@
+'''
+#Exercício 1
+num1= int(input("Escreva um numero: "))
+num2= int(input("Escreva outro numero: "))
+if num1>num2:
+    print(f"O maior número é {num1}")
+else:
+    print(f"O maior número é {num2}")
+
+//////////////////////////////
+#Exercício 2
+ano = int(input("Insira seu ano de nascimento: "))
+anoat = 2024
+idade = anoat - ano
+if idade>=18:
+    print("Você vota esse ano...")
+else:
+    print("Você não vota seu besta")'''
+#Exercício 3
+
+'''senha = input("Insira a senha: ")
+senha_correta = '1234'
+if senha == senha_correta:
+    print("ACESSO PERMITIDO")
+else:
+    print("ACESSO NEGADO")'''
+
+#Exercício 4
+'''qtd = int(input("Quantas maçãs você comprou branca de neve?: "))
+if qtd < 12:
+    m = 0.30
+else:
+    m = 0.25
+valor_final = qtd * m 
+print(f"O valor total é de R$: {valor_final}")
+'''
+
+#Exercício 5
+a = int(input("Valor 1: "))
+b = int(input("Valor 2: "))
+c = int(input("Valor 3: "))
